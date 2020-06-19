e43ca2f31e10ba24b3e2bbe90dbda0d38fc74db9
alan <alan.www@outlook.com>
2020-06-19 18:03:13 +0300

    basic markup pages added

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pizza | Cart</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand">Pizza Shop</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Menu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">My Orders</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                        test@mail.ru
+                    </span>
+                    <button class="btn btn-outline-success ml-2" type="button">Cart (0)</button>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12">
+            <h2 class="text-center">Cart</h2>
+
+            <div class="card mb-2 p-2">
+                <div class="row no-gutters d-flex align-items-center">
+
+                    <div class="col-md-1">
+                        <img src="https://via.placeholder.com/350x350" class="card-img-top" alt="..." style="width: 100%;">
+                    </div>
+
+                    <div class="col-md-9 pl-2">
+                        <h5 class="card-title" style="margin-bottom: 0;">Название карточки</h5>
+                        <p class="card-text mb-0">20$ a piece</p>
+                    </div>
+
+                    <div class="col-md-2">
+
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon1">-</button>
+                            </div>
+                            <input type="text" class="form-control" style="border-color: #6c757d; border-right: none;">
+                            <div class="input-group-prepend">
+
+                            </div>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon1">+</button>
+                                <button class="btn btn-outline-danger" type="button" id="button-addon1">Delete</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-2 p-2">
+                <div class="row no-gutters d-flex align-items-center">
+
+                    <div class="col-md-1">
+                        <img src="https://via.placeholder.com/350x350" class="card-img-top" alt="..." style="width: 100%;">
+                    </div>
+
+                    <div class="col-md-9 pl-2">
+                        <h5 class="card-title" style="margin-bottom: 0;">Название карточки</h5>
+                        <p class="card-text mb-0">20$ a piece</p>
+                    </div>
+
+                    <div class="col-md-2">
+
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon1">-</button>
+                            </div>
+                            <input type="text" class="form-control" style="border-color: #6c757d; border-right: none;">
+                            <div class="input-group-prepend">
+
+                            </div>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon1">+</button>
+                                <button class="btn btn-outline-danger" type="button" id="button-addon1">Delete</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-2 p-2">
+                <div class="row no-gutters d-flex align-items-center">
+
+                    <div class="col-md-1">
+                        <img src="https://via.placeholder.com/350x350" class="card-img-top" alt="..." style="width: 100%;">
+                    </div>
+
+                    <div class="col-md-9 pl-2">
+                        <h5 class="card-title" style="margin-bottom: 0;">Название карточки</h5>
+                        <p class="card-text mb-0">20$ a piece</p>
+                    </div>
+
+                    <div class="col-md-2">
+
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon1">-</button>
+                            </div>
+                            <input type="text" class="form-control" style="border-color: #6c757d; border-right: none;">
+                            <div class="input-group-prepend">
+
+                            </div>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon1">+</button>
+                                <button class="btn btn-outline-danger" type="button" id="button-addon1">Delete</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-2 p-2">
+                <div class="row no-gutters d-flex align-items-center">
+
+                    <div class="col-md-1">
+                        <img src="https://via.placeholder.com/350x350" class="card-img-top" alt="..." style="width: 100%;">
+                    </div>
+
+                    <div class="col-md-9 pl-2">
+                        <h5 class="card-title" style="margin-bottom: 0;">Название карточки</h5>
+                        <p class="card-text mb-0">20$ a piece</p>
+                    </div>
+
+                    <div class="col-md-2">
+
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon1">-</button>
+                            </div>
+                            <input type="text" class="form-control" style="border-color: #6c757d; border-right: none;">
+                            <div class="input-group-prepend">
+
+                            </div>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon1">+</button>
+                                <button class="btn btn-outline-danger" type="button" id="button-addon1">Delete</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-md-12">
+            <p class="text-right"><span>500</span> + <span>200 (Delivery)</span> </p>
+            <p class="text-right">
+                <span class="h2 font-weight-bold">Total: <span>700€</span></span>
+                <span class="h2 font-weight-bold text-secondary">(<span>800$</span>)</span>
+            </p>
+
+        </div>
+    </div>
+
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pizza</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand">Pizza Shop</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Menu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">My Orders</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                        test@mail.ru
+                    </span>
+                    <button class="btn btn-outline-success ml-2" type="button">Cart (0)</button>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12">
+            <h2 class="text-center">Menu</h2>
+
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="row d-flex flex-row justify-content-start flex-wrap pt-3">
+                        <div class="col-lg-3 col-md-4 col-sm-6 mb-5">
+                            <div class="card h-100">
+                                <img src="https://via.placeholder.com/350x350" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Название карточки</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                                <ul class="list-group list-group-flush" style="border-bottom: none;">
+                                    <li class="list-group-item">10$</li>
+                                </ul>
+                                <div class="card-footer text-muted">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1">-</span>
+                                        </div>
+                                        <input type="text" class="form-control" value="0">
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">+</span>
+                                            <span class="input-group-text">Add to cart</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+
+                </div>
+                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
